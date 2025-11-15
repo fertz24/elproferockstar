5d53c10fd80cef5c9ef71c7b160eed2a92de640f
GARCIA BORBOA BRIANNA FERNANDA <briannafernanda4@gmail.com>
2025-11-14 20:52:14 -0800

    avances del dia 14 de noviembre

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:cinegod/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinegod/presentation/views/movies/home_view.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
@@ -9,36 +8,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Example of accessing another environment variable
-    return const Scaffold(body: _HomeView());
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget { //referncia del riverpod con el cambio de estado
-  const _HomeView();
-
-  @override
-  ConsumerState<_HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //se cargan los valores que va a recibir, la lista de movies
-      if (nowPlayingMovies.isEmpty) {
-      return const Center(child: CircularProgressIndicator());
-    }
-    return ListView.builder(
-      itemBuilder: (_, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(title: Text(movie.title));
-      },
-      itemCount: nowPlayingMovies.length,
-    );
+    return const Scaffold(body: SafeArea(child: HomeView()));
   }
 }
---
+++ lib/presentation/views/movies/home_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinegod/presentation/providers/movies/movies_providers.dart';
+
+  class HomeView extends ConsumerStatefulWidget{
+
+  static const String routeName = 'home_screen';
+  const HomeView({super.key});
+
+  @override
+  ConsumerState<HomeView> createState() => _HomeViewState();
+
+  }
+
+  class _HomeViewState extends ConsumerState<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //se cargan los valores que va a recibir, la lista de movies
+      if (nowPlayingMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return ListView.builder(
+      itemBuilder: (_, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(title: Text(movie.title));
+      },
+      itemCount: nowPlayingMovies.length,
+    );
+  }
+}
